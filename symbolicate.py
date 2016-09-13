9420d9529b347a7189db49f1752c36f453251d83
James Howard <jameshoward@mac.com>
2016-09-13 10:49:12 -0700

    Add support for iOS as well as symbolicating from dsyms as found by spotlight

--- symbolicate.py
+++ symbolicate.py
@@ -4,6 +4,8 @@ import fileinput
 import re
 import sys
 import subprocess
+import traceback
+import uuid as _uuid
 
 class BinaryImage:
     def __init__(self, lowAddr, hiAddr, name, uuid, path, arch='x86_64'):
@@ -12,15 +14,28 @@ class BinaryImage:
         self.name = name
         self.uuid = uuid
         self.path = path
-        self.arch = arch
+        self.arch = arch if arch != "x86-64" else "x86_64" # sigh
     
     def match(self, addr):
         return self.lowAddr <= addr and self.hiAddr >= addr
         
     def symbolicate(self, addresses):
+        if not hasattr(self, 'dsymPath'):
+            findArgs = ['/usr/bin/mdfind', 'com_apple_xcode_dsym_uuids == %s' % (str(_uuid.UUID(self.uuid)).upper())]
+            try:
+                findResult = subprocess.check_output(findArgs).split('\n')
+                if len(findResult) >= 1 and len(findResult[0].strip()) > 0:
+                    self.dsymPath = findResult[0] + "/Contents/Resources/DWARF/%s" % self.name
+                else:
+                    self.dsymPath = None
+            except:
+                self.dsymPath = None
+    
+        path = self.dsymPath if self.dsymPath is not None else self.path
+
         args =  ['/usr/bin/xcrun', 
                   'atos', 
-                  '-o', self.path, 
+                  '-o', path, 
                   '-arch', self.arch, 
                   '-l', ("0x%x"%self.lowAddr)]
         
@@ -44,7 +59,13 @@ def seek(pattern):
         if (re.match(pattern, line)):
             return i
     return None
-    
+
+# find architecture
+loc = seek(r'Code Type:')
+if not loc:
+    sys.stderr.write("Cannot find architecture (Code Type:). Maybe this isn't a crash file?\n")
+    sys.exit(1)
+arch = re.match(r'Code Type:\s+([\w\d\_\-\.]+).*', lines[loc-1]).group(1).lower()
     
 # find binary images section
 loc = seek(r'Binary Images:')
@@ -52,7 +73,7 @@ if not loc:
     sys.stderr.write("Cannot find Binary Images:. Maybe this isn't a crash file?\n")
     sys.exit(1)        
 
-imageRE = re.compile(r'\s*(0x[0-9a-f]+)\s*-\s*(0x[0-9a-f]+)\s*(.*?)\s*<([0-9A-F\-]+)>\s*(.*)\n')
+imageRE = re.compile(r'\s*(0x[0-9a-f]+)\s*-\s*(0x[0-9a-f]+)\s*(.*?)\s*<([0-9A-Fa-f\-]+)>\s*(.*)\n')
 images = []
 for line in lines[loc:]:
     if len(line) == 0:
@@ -61,10 +82,14 @@ for line in lines[loc:]:
     if match:
         lowAddr = int(match.group(1), 16)
         hiAddr = int(match.group(2), 16)
-        name = match.group(3)
+        nameParts = match.group(3).split(" ")
+        name = nameParts[0]
+        imgArch = arch
+        if len(nameParts) > 1 and not nameParts[1].startswith("("):
+            imgArch = nameParts[1]
         uuid = match.group(4)
         path = match.group(5)
-        image = BinaryImage(lowAddr, hiAddr, name, uuid, path)
+        image = BinaryImage(lowAddr, hiAddr, name, uuid, path, imgArch)
 #        sys.stderr.write("Found image %s\n" % str(image))
         images.append(image)        
         
@@ -73,7 +98,7 @@ for line in lines[loc:]:
 addrsByImage = {}
 addrToSymbol = {}
 
-btRE = re.compile(r'(\d+\s*[\w\.]+\s*)(0x[0-9a-f]+)\s(.*)\n')
+btRE = re.compile(r'(\d+\s*[\w\d\-\_\.]+\s*)(0x[0-9a-f]+)\s(.*)\n')
 for line in lines:
     match = btRE.match(line)
     if match:
@@ -84,7 +109,7 @@ for line in lines:
         for image in images:
             symbolicated = None
             if image.match(addr):
-                if addrsByImage.has_key(image):
+                if image in addrsByImage:
                     addrsByImage[image].append(addr)
                 else:
                     addrsByImage[image] = [addr]
@@ -107,10 +132,10 @@ for line in lines:
         addr = int(match.group(2), 16)
         suffix = match.group(3)
         
-        if addrToSymbol.has_key(addr):
+        if addr in addrToSymbol:
             symbol = addrToSymbol[addr]
             newLine = "%s0x%x %s\n" % (prefix, addr, symbol)
-    
+
     sys.stdout.write(newLine)
 
                                 
